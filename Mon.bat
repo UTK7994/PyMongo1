cfab81fd8a9941d43d19b551cf47d2019e7a0ce9
Utkarsh Gupta <UTK7994@users.noreply.github.com>
2015-01-04 22:43:45 +0530

    Create Mon.bat

---
+++ Mon.bat
@@ -0,0 +1,8 @@
+REM THE name of my computer is crysis and project was on desktop so name the path accordingly
+@echo off
+color 0e
+cd/
+cd C:\Users\CrysiS\Desktop
+mongo < "c:\\users\\crysis\\desktop\\Mini-project\\mongo.js"
+cd C:\Users\CrysiS\Desktop\Mini-project
+color 07
